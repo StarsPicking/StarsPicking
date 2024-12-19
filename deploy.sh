5f98854d64c4b3a0de762677689572d694cc41db
zhangtq <15652533044@163.com>
2024-12-19 13:56:14 +0800

    deploy, heads/master-0-g350df37

--- deploy.sh
+++ deploy.sh
@@ -3,15 +3,15 @@
 # 确保脚本抛出遇到的错误
 set -e
 
-dist_path=./pubic
+dist_path=./public
 repo_name=hexo_theme_next
 # 生成静态文件
 npm run 
 
 # 进入生成的文件夹打包
-cd $dist_path
+# cd $dist_path
 echo $dist_path
-tar --exclude=dist/.git -zcvf dist.tar.gz &dist_path
+tar --exclude=$dist_path/.git -zcvf dist.tar.gz $dist_path
 
 #上传至个人服务器
 scp -P 22 -r dist.tar.gz root@www.zhangtq.com:/var/www/$repo_name/
