350df375494b9e435e03f356956df44076f2d63a
zhangtq <15652533044@163.com>
2024-12-19 13:13:11 +0800

    '添加一键部署脚本'

---
+++ deploy.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env sh
+
+# 确保脚本抛出遇到的错误
+set -e
+
+dist_path=./pubic
+repo_name=hexo_theme_next
+# 生成静态文件
+npm run 
+
+# 进入生成的文件夹打包
+cd $dist_path
+echo $dist_path
+tar --exclude=dist/.git -zcvf dist.tar.gz &dist_path
+
+#上传至个人服务器
+scp -P 22 -r dist.tar.gz root@www.zhangtq.com:/var/www/$repo_name/
+ssh root@www.zhangtq.com -p 22 "cd /var/www/$repo_name/ && tar -xvf dist.tar.gz && rm -f dist.tar.gz"
+# rm -f dist.tar.gz && rm -rf dist/
+
+# 将代码同步更新到gitee, github
+git add .
+commit_info=`git describe --all --always --long`
+git commit -m "deploy, $commit_info"
+git push origin master
+ssh zhangtianqing@www.zhangtq.com -p 22 "
+cd /home/zhangtianqing/$repo_name
+git checkout master
+git pull gitee master
+git checkout main
+git merge master
+git push github main
+git checkout master
+"
