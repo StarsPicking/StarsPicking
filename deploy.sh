2c0a2bf9a57a2eeedbc6c33164bdcbb9ae68ae1a
zhangtq <15652533044@163.com>
2024-12-19 14:38:48 +0800

    deploy, heads/master-0-gaad85f2

--- deploy.sh
+++ deploy.sh
@@ -11,7 +11,7 @@ npm run
 # 进入生成的文件夹打包
 # cd $dist_path
 echo $dist_path
-tar --exclude=$dist_path/.git -zcvf dist.tar.gz $dist_path
+tar --exclude=$dist_path/.git -zcvf dist.tar.gz $dist_path/*
 
 #上传至个人服务器
 scp -P 22 -r dist.tar.gz root@www.zhangtq.com:/var/www/$repo_name/dist
